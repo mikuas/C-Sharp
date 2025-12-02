ee8b949607ebf1731b0a39dee37096920b63b233
mikuas <none>
2025-12-02 22:46:01 +0800

    Namespace, OperatorOverload, Polymorphism, PreprocessorInstructions, Interface, Inheritance, Class

--- MyConsoleApp/ClassApplication/Class.cs
+++ MyConsoleApp/ClassApplication/Class.cs
@@ -3,6 +3,18 @@
 namespace MyConsoleApp.ClassApplication;
 
 
+// 抽象类 abstract
+public abstract class Test1
+{
+    // 抽象类不能被实例化
+}
+
+// 密封类 sealed
+public sealed class Test2
+{
+    // 密封类不能被继承
+}
+
 // 定义一个类, 类的默认访问标识符是 internal
 public class Class
 {
@@ -10,8 +22,10 @@ public class Class
     public double Length;
     // 默认成员变量标识符是 private
     double _height;
-    // 静态成员
+
     public int Count;
+    // 静态成员
+    public static int StaticCount;
 
     // 无参构造函数
     public Class()
@@ -40,12 +54,46 @@ public class Class
         Count++;
     }
 
+    public void StaticAdd()
+    {
+        StaticCount++;
+    }
+
     public int GetCount() => Count;
+
+    // static方法只能访问static成员变量
+    public static void StaticFunc()
+    {
+        Console.WriteLine(StaticCount);
+    }
     
+    public int GetStaticCount() => StaticCount;
+
     public static void Demo()
     {
         Class obj1 = new Class();
         Class obj2 = new Class(10, 24);
+
+        Class test1 = new Class();
+        Class test2 = new Class();
+        
+        test1.Add();
+        test1.Add();
+        test1.Add();
+        test1.StaticAdd();
+        test1.StaticAdd();
+        test1.StaticAdd();
+        
+        test2.Add();
+        test2.Add();
+        test2.Add();
+        test2.Add();
+        test2.StaticAdd();
+        test2.StaticAdd();
+        test2.StaticAdd();
+        
+        Console.WriteLine($"Count: {test1.GetCount()}, StaticCount: {test1.GetStaticCount()}");
+        Console.WriteLine($"Count: {test2.GetCount()}, StaticCount: {test2.GetStaticCount()}");
     }
 }
 
