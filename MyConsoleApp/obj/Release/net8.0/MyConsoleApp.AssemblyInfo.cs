a67a1cb33974f74a4bd0aed41383f4ccd6d681ef
mikuas <none>
2025-12-17 23:30:24 +0800

    Changes

--- MyConsoleApp/obj/Release/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Release/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b13ef6899f8864b9e2a66776088a2f843b625f94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89c7b05ff9b8fe8c1cd80ebc2612d9c101b93d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
