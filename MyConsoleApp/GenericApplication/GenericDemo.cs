650df93948854b1c0455d6331de66194b2d72383
mikuas <none>
2025-12-22 18:09:48 +0800

    Generic(泛型)

---
+++ MyConsoleApp/GenericApplication/GenericDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace MyConsoleApp.GenericApplication;
+
+public class GenericDemo
+{
+    public class MyGenericArray<T> where T : new() // 泛型约束
+    {
+        private T[] _array;
+
+        public MyGenericArray(int size)
+        {
+            _array = new T[size];
+        }
+        
+        public T GetItem(int index ) => _array[index];
+        public void SetItem(int index, T value) => _array[index] = value;
+        public T[] GetArray() => _array;
+        public override string ToString() => string.Join(", ", _array);
+    }
+
+    public static void Demo()
+    {
+        MyGenericArray<int> array = new MyGenericArray<int>(10);
+
+        for (int i = 0; i < 10; i++)
+        {
+            array.SetItem(i, i);
+        }
+
+        Console.WriteLine(array);
+    }
+}
