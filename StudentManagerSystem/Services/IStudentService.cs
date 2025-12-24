5de3678df7a4938956d4d8a695c52bd9573db9fc
mikuas <none>
2025-12-24 23:41:44 +0800

    IStudentService(学生服务接口)

---
+++ StudentManagerSystem/Services/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagerSystem.Services;
+
+public interface IStudentService
+{
+    void AddStudent();
+    void QueryStudent();
+    void UpdateStudent();
+    void DeleteStudent();
+}
