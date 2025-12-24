03c9894a402ea2b879e95fb07fcf6ac216c5d633
mikuas <none>
2025-12-24 23:42:12 +0800

    JsonStudentStorage(保存学生数据到Json文件)

---
+++ StudentManagerSystem/Storage/JsonStudentStorage.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using StudentManagerSystem.Models;
+
+
+namespace StudentManagerSystem.Storage;
+
+public class JsonStudentStorage
+{
+    private readonly string _path;
+    
+    public JsonStudentStorage(string path)
+    {
+        _path = path;
+    }
+    
+    public void Save(Dictionary<string, Student> dict)
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(_path)!);
+
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        
+        File.WriteAllText(_path, JsonSerializer.Serialize(dict, options));
+    }
+
+    public Dictionary<string, Student> Load()
+    {
+        if (!File.Exists(_path))
+        {
+            return new Dictionary<string, Student>();
+        }
+        
+        return JsonSerializer.Deserialize<Dictionary<string, Student>>(File.ReadAllText(_path)) ?? new Dictionary<string, Student>();
+    }
+}
