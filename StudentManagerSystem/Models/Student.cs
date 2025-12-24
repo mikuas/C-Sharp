7f4fbcc93030b31a3d36537a8f9c60c9caabd15e
mikuas <none>
2025-12-24 23:42:47 +0800

    Student(学生类)

---
+++ StudentManagerSystem/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentManagerSystem.Models;
+
+public class Student
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required char Gender { get; set; }
+    public required int Age { get; set; }
+
+    public override string ToString() => $"学号: {Id},\t 姓名: {Name},\t 性别: {Gender},\t 年龄: {Age}";
+}
